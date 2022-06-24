f9b192533eb5badb5ed9f47c90e91d671b72c355
Jared Cosulich <jared.cosulich@gmail.com>
2022-06-24 17:42:49 -0400

    mark proxy provider as ethos

--- src/lib/getProvider.ts
+++ src/lib/getProvider.ts
@@ -4,12 +4,14 @@ import activeUser from './activeUser'
 import getConfiguration from './getConfiguration'
 import networkToChain from './networkToChain'
 
-const getProvider = (network?: Networkish): ethers.providers.Web3Provider => {
+const getProvider = async (network?: Networkish): Promise<ethers.providers.Web3Provider> => {
   const { appId, walletAppUrl, network: defaultNetwork } = getConfiguration()
   const rpcProvider = new ethers.providers.JsonRpcProvider(
     walletAppUrl + '/api/rpc',
     defaultNetwork
   )
+
+  const user: any = await activeUser(appId)
 
   const signer = rpcProvider.getSigner()
   const proxySigner = new Proxy(signer, {
@@ -19,7 +21,6 @@ const getProvider = (network?: Networkish): ethers.providers.Web3Provider => {
           return true
         case 'getAddress':
           return async () => {
-            const user: any = await activeUser(appId)
             const networkString = networkToChain((network || defaultNetwork).toString())
             return user.accounts.find((account: any) => account.chain === networkString)?.address
           }
@@ -32,10 +33,12 @@ const getProvider = (network?: Networkish): ethers.providers.Web3Provider => {
   return new Proxy(rpcProvider, {
     get: (target: any, prop: any, receiver: any) => {
       switch (prop) {
+        case 'ethos':
+          return true
         case 'getNetwork':
           return () => network
         case 'getSigner':
-          return () => proxySigner
+          return () => (user ? proxySigner : undefined)
         default:
           return Reflect.get(target, prop, receiver)
       }
