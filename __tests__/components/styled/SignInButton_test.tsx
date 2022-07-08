df0635115dcc274611e595ef5f39dcb89bd61682
Jared Cosulich <jared.cosulich@gmail.com>
2022-07-08 08:32:12 -0400

    testing signin button

--- __tests__/components/styled/SignInButton_test.tsx
+++ __tests__/components/styled/SignInButton_test.tsx
@@ -1,8 +1,10 @@
-import SignInButton from '../../../src/components/styled/SignInButton'
-import Button from '../../../src/components/headless/Button'
-
 import React from 'react';
 import { create, act } from 'react-test-renderer';
+
+import SignInButton from '../../../src/components/styled/SignInButton'
+import SignInModal from '../../../src/components/styled/SignInModal';
+import Button from '../../../src/components/headless/Button'
+
 
 describe("SignInButton", () => {
   it('renders correctly', () => {
@@ -21,12 +23,25 @@ describe("SignInButton", () => {
     )
     const testInstance = signInButton.root;
     expect(testInstance.findAllByProps({ isOpen: true }).length).toBe(0)
+    expect(onClick.mock.calls.length).toBe(0)
 
     act(() => {
       testInstance.findByType(Button).props.onClick();
     })
 
     expect(testInstance.findAllByProps({ isOpen: true }).length).toBe(1)
-    expect(signInButton.toJSON()).toMatchSnapshot();  
-});
+    expect(onClick.mock.calls.length).toBe(1)
+  });
+
+  it('passes along onEmailSent to the signInModal', () => {
+    const onEmailSent = jest.fn();
+    const signInButton = create(
+      <SignInButton 
+        onEmailSent={onEmailSent}
+      />
+    )
+    const testInstance = signInButton.root;
+    const modal = testInstance.findByType(SignInModal)
+    expect(modal.props.onEmailSent).toBe(onEmailSent)
+  })
 });
