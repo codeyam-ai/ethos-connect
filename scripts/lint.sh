7ee101275ebae8870f910a0034bd92a43710eddb
Jared Cosulich <jared.cosulich@gmail.com>
2022-07-23 10:48:56 -0400

    remove lint for now

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,35 +1,35 @@
-#!/usr/bin/env bash
-set -e
+# #!/usr/bin/env bash
+# set -e
 
-ROOT_DIR="$(git rev-parse --show-toplevel)/"
-TARGET_DIR="$(pwd)"
-RELATIVE_TARGET_DIR="${TARGET_DIR/$ROOT_DIR/}"
+# ROOT_DIR="$(git rev-parse --show-toplevel)/"
+# TARGET_DIR="$(pwd)"
+# RELATIVE_TARGET_DIR="${TARGET_DIR/$ROOT_DIR/}"
 
-# INFO: This script is always run from the root of the repository. If we execute this script from a
-# package then the filters (in this case a path to $RELATIVE_TARGET_DIR) will be applied.
+# # INFO: This script is always run from the root of the repository. If we execute this script from a
+# # package then the filters (in this case a path to $RELATIVE_TARGET_DIR) will be applied.
 
-pushd $ROOT_DIR > /dev/null
+# pushd $ROOT_DIR > /dev/null
 
-prettierArgs=()
+# prettierArgs=()
 
-if ! [ -z "$CI" ]; then
-  prettierArgs+=("--check")
-else
-  prettierArgs+=("--write")
-fi
+# if ! [ -z "$CI" ]; then
+#   prettierArgs+=("--check")
+# else
+#   prettierArgs+=("--write")
+# fi
 
-# Add default arguments
-prettierArgs+=('--ignore-unknown')
+# # Add default arguments
+# prettierArgs+=('--ignore-unknown')
 
-# Passthrough arguments and flags
-prettierArgs+=($@)
+# # Passthrough arguments and flags
+# prettierArgs+=($@)
 
-# Ensure that a path is passed, otherwise default to the current directory
-if [ -z "$@" ]; then
-  prettierArgs+=("$RELATIVE_TARGET_DIR")
-fi
+# # Ensure that a path is passed, otherwise default to the current directory
+# if [ -z "$@" ]; then
+#   prettierArgs+=("$RELATIVE_TARGET_DIR")
+# fi
 
-# Execute
-yarn prettier "${prettierArgs[@]}"
+# # Execute
+# yarn prettier "${prettierArgs[@]}"
 
-popd > /dev/null
+# popd > /dev/null
