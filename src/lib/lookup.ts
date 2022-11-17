a436444ab71814a53507835c97af4c5fdb4c528d
Jared Cosulich <jared.cosulich@gmail.com>
2022-11-17 09:38:56 -0500

    fixing suins lookup

--- src/lib/lookup.ts
+++ src/lib/lookup.ts
@@ -8,28 +8,28 @@ const CACHE_DELAY = 1000 * 30;
 const lookup = async (nameOrAddress: string): Promise<SuiAddress | string> => {
     const provider = new JsonRpcProvider(Network.DEVNET);
 
-    const nameObjectId = '0xfec9c7ccea19fedec31fc5ed4ecb968e7cc7ecac';
+    const namesObjectId = '0xfec9c7ccea19fedec31fc5ed4ecb968e7cc7ecac';
 
     const lookupStore = store.namespace('lookup')
-    const recordsInfo = await lookupStore(nameObjectId);
+    const recordsInfo = await lookupStore('suins');
 
     const { version, timestamp } = recordsInfo || {};
     let { suiNSRecords } = recordsInfo || {};
 
     if (suiNSRecords && Date.now() - timestamp > CACHE_DELAY) {
-        const ref = await provider.getObjectRef(nameObjectId);
+        const ref = await provider.getObjectRef(namesObjectId);
         if (ref) {
             if (version !== getObjectVersion(ref)) {
                 suiNSRecords = null;
             }
         }
         recordsInfo.timestamp = Date.now();
-        lookupStore(nameObjectId, recordsInfo);
+        lookupStore('suins', recordsInfo);
     }
 
     if (!suiNSRecords) {
-        const namesObject = await provider.getObject(nameObjectId);
-        if (namesObject.status === 'Exists') {
+        const namesObject = await provider.getObject(namesObjectId);
+       if (namesObject.status === 'Exists') {
             const suiNamesObject = namesObject.details as SuiObject;
             const moveNameObject = suiNamesObject.data as SuiMoveObject;
             const records = moveNameObject.fields.records.fields.contents;
