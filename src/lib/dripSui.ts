2ab3e99c45637281eba20bf34b3755d4058c0c4d
Adam Wade <adamwade2384@gmail.com>
2023-02-23 13:29:09 -0800

    fix: network for dripSui request (devnet)
    Allow optional network for games such as Sui8192, Checkers, Chess. Network passed in must match Mysten/Sui.js Network.DEVNET other wise pass empty response with error notifying the user to use Devnet.

--- src/lib/dripSui.ts
+++ src/lib/dripSui.ts
@@ -1,12 +1,24 @@
-import { JsonRpcProvider } from "@mysten/sui.js";
+import { JsonRpcProvider } from '@mysten/sui.js';
 import { DEFAULT_NETWORK } from './constants';
 
 type DripSuiProps = {
   address: string,
-  network: string
+  network?: string
 }
 
 const dripSui = async ({ address, network }: DripSuiProps) => {
+  // If a network is included in the request
+  // make sure it's the DEVNET network.
+  if(network && network !== DEFAULT_NETWORK) {
+    return {
+      transferred_gas_objects: [],
+      error:
+        'The network you provided does not support the faucet. Please use the DEVNET network.',
+    };
+  }
+
+  // Otherwise, request SUI from the faucet using the given network. 
+  // If no network is provided, the default network will be used (devnet).
   const provider = new JsonRpcProvider(network || DEFAULT_NETWORK);
   return provider.requestSuiFromFaucet(address)
 }
