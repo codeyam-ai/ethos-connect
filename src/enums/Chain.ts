8eb0f158b8bf3d1a417c466a711fb936c175650e
Jared Cosulich <jared.cosulich@gmail.com>
2023-04-03 14:17:03 -0400

    fixing network identifier strings

--- src/enums/Chain.ts
+++ src/enums/Chain.ts
@@ -1,5 +1,5 @@
 export enum Chain {
-  SUI_DEVNET = 'sui::devnet',
-  SUI_TESTNET = 'sui::testnet',
-  SUI_CUSTOM = 'sui::custom',
+  SUI_DEVNET = 'sui:devnet',
+  SUI_TESTNET = 'sui:testnet',
+  SUI_CUSTOM = 'sui:custom',
 }
