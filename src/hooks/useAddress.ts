f1804eaefec5705fd1d30c4fc13e851c47044fd2
Jared Cosulich <jared.cosulich@gmail.com>
2023-04-10 20:38:34 -0400

    refactor: useAddress

--- src/hooks/useAddress.ts
+++ src/hooks/useAddress.ts
@@ -1,16 +1,9 @@
-import { useEffect, useState } from "react";
 import useProviderAndSigner from "./useProviderAndSigner";
 
 const useAddress = () => {
     const { signer } = useProviderAndSigner();
-    const [address, setAddress] = useState<string | null>(null);
 
-    useEffect(() => {
-        if (!signer?.currentAccount) return;
-        setAddress(signer.currentAccount.address)
-    }, [signer]);
-
-    return address;
+    return signer?.currentAccount?.address;
 }
 
 export default useAddress;
