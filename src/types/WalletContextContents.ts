ca51d2628d0eec14bd9982635d973d93c1a895f0
Austin Gatlin <austin@gatlin.io>
2023-08-02 10:56:19 +1000

    fix: replace ProviderAndSigner with ClientAndSigner

--- src/types/WalletContextContents.ts
+++ src/types/WalletContextContents.ts
@@ -1,14 +1,14 @@
-import { JsonRpcProvider } from '@mysten/sui.js';
 import type { WalletAdapter } from "@mysten/wallet-adapter-base";
 import { Wallet } from './Wallet';
 import { EthosConnectStatus } from '../enums/EthosConnectStatus';
 import { WalletAccount } from '@mysten/wallet-standard';
+import {SuiClient} from '@mysten/sui.js/client'
 
 export type WalletContextContents = {
     wallets?: WalletAdapter[],
     selectWallet?: ((walletName: string) => void),
     status: EthosConnectStatus,
-    provider: JsonRpcProvider | null,
+    client: SuiClient | null,
     wallet?: Wallet;
     altAccount?: WalletAccount;
     setAltAccount: (_account: WalletAccount) => void;
