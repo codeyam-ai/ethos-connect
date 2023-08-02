95917b28d04c2a491e76092354e42041597e6d22
Austin Gatlin <austin@gatlin.io>
2023-08-03 09:22:03 +1000

    fix: lib.nameService to use client instead of JSONRpcProvider

--- src/lib/nameService.ts
+++ src/lib/nameService.ts
@@ -1,21 +1,12 @@
-import { Connection, JsonRpcProvider } from '@mysten/sui.js';
-import { DEFAULT_NETWORK } from './constants';
+import { SuiClient } from '@mysten/sui.js/client'
+import { DEFAULT_NETWORK } from './constants'
 
 export const getSuiName = async (address: string, network?: string) => {
-  const connection = new Connection({ fullnode: network ?? DEFAULT_NETWORK })
-  const suiProvider = new JsonRpcProvider(connection);
-
-  // const x = await suiProvider.resolveNameServiceNames({ address: '0x22e544fc7bfe271395ac7955c21d7db62734bcadabfee17c0c2ec9359172d90c' });
-  // console.log('x', x, address, connection.fullnode);
-  // const z = await suiProvider.resolveNameServiceAddress({ name: 'irrational.sui' });
-  // console.log('z', z);
-
-  return suiProvider.resolveNameServiceNames({ address });
-};
+  const client = new SuiClient({ url: network ?? DEFAULT_NETWORK })
+  return client.resolveNameServiceNames({ address })
+}
 
 export const getSuiAddress = async (name: string, network?: string) => {
-  const connection = new Connection({ fullnode: network ?? DEFAULT_NETWORK })
-  const suiProvider = new JsonRpcProvider(connection);
-
-  return suiProvider.resolveNameServiceAddress({ name });
-};
+  const client = new SuiClient({ url: network ?? DEFAULT_NETWORK })
+  return client.resolveNameServiceAddress({ name })
+}
